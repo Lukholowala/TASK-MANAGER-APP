d22ce2de0cee4c793ad6d0912c0dd365537b856d
lukholo <lukhowala@gmail.com>
2025-06-03 20:03:21 +0200

    Configured MongoDB
    connected mongoDB to the server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -13,8 +14,13 @@ app.use(
     })
 );
 
+//Connect
+connectDB();
+
 // Middleware
 app.use(express.json());
+
+
 
 //Routes
 // app.use("/api/auth", authRoutes);
