cfe9026681d8d06281a3829032ea40f34ce9af43
lukholo <lukhowala@gmail.com>
2025-06-06 00:40:49 +0200

    Authentication APIs
    Login, Sign-Up, Get User Info and Profile image upload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const {registerUser, loginUser, getUserProfile, updateUserProfile} = require ("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/uploadMiddleware");
 
 const router = express.Router();
 
@@ -8,4 +11,11 @@ router.post("/login", loginUser); // Login User
 router.get("/profile", protect, getUserProfile); // Get User Profile
 router.put("/profile", protect, updateUserProfile); // Update Profile
 
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file Uploaded "});
+    }
+    const imageUrl =  `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    res.status(200).json({imageUrl});
+}); 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // Middleware to handle CORS
@@ -23,7 +25,7 @@ app.use(express.json());
 
 
 //Routes
-// app.use("/api/auth", authRoutes);
+ app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
 // app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
