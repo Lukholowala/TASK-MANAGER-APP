ee08807da2fd36fa3bb50fec62e90a519b10b962
lukholo <lukhowala@gmail.com>
2025-07-25 02:26:37 +0200

    Dashboardsummary section
    Total tasks, Pending tasks, In progress tasks, Completed tasks

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -7,6 +7,9 @@ import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import moment from 'moment'
+import { addThousandsSeparator } from "../../utils/helper";
+import  InfoCard  from "../../components/Cards/InfoCard"
+
 
 const Dashboard = () => {
     useUserAuth();
@@ -48,8 +51,42 @@ const Dashboard = () => {
                 </p>
             </div>
         </div>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+            <InfoCard
+                label="Total Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.All || 0
+                )}
+                color="bg-blue-700"
+            />
 
+            <InfoCard
+                label="Pending Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.Pending || 0
+                )}
+                color="bg-violet-500"
+            />
+
+             <InfoCard
+                label="In Progress Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.InProgress || 0
+                )}
+                color="bg-cyan-500"
+            />
+
+             <InfoCard
+                label="Completed Tasks"
+                value={addThousandsSeparator(
+                    dashboardData?.charts?.taskDistribution?.Completed || 0
+                )}
+                color="bg-lime-500"
+            />
+        </div>
        </div>
+
+       
     </DashboardLayout>;
 };
 
